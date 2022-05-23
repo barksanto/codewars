0c210cc1f76d120dee94fab87286692e593b073d
Barkley <barkley.santo@gmail.com>
2022-05-23 09:47:38 -0500

    challenge complete

---
+++ HighestScoreWord/highest.js
@@ -0,0 +1,25 @@
+function high(x) {
+  const alphabet = "abcdefghijklmnopqrstuvwxyz";
+  const words = x.split(" ");
+  let result = words[0];
+  let maxScore = 0;
+
+  words.forEach((word) => {
+    let score = 0;
+
+    for (var i = 0; i < word.length; i++) {
+      score += alphabet.indexOf(word[i]) + 1;
+    }
+
+    if (score > maxScore) {
+      maxScore = score;
+      result = word;
+    }
+  });
+  console.log(result);
+  return result;
+}
+high("man i need a taxi up to ubud");
+high("what time are we climbing up the volcano");
+// high("take me to semynak");
+// high("aa b");
