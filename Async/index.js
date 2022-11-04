f3abab05f135aa04f7188f6c83d26a5457f11b0d
Barkley <barkley.santo@gmail.com>
2022-11-04 12:17:50 +0700

    pending promises

--- Async/index.js
+++ Async/index.js
@@ -1,18 +1,26 @@
-const fetchData = () => {
-  const promise = new Promise((res, rej) => {
-    setTimeout(() => {
-      res("Done!")
-    }, 2000)
-  })
-  return promise
+const regularFetch = () => {
+  return fetch("https://jsonplaceholder.typicode.com/posts/1")
+    .then((response) => response.json())
+    .then((json) => console.log(json))
+    .catch((err) => console.log(err))
 }
 
-setTimeout(() => {
-  console.log("Timer is done")
-  fetchData().then((text) => {
-    console.log(text)
-  })
-}, 2000)
+// regularFetch()
 
-// console.log("hello")
-// console.log("hiii")
+// Async/Await API Call
+
+async function asyncApiCall() {
+  try {
+    const response = await fetch("https://jsonplaceholder.typicode.com/posts/1")
+    // console.log(response.ok)
+    const data = await response.json()
+    // console.log(data)
+    return data
+  } catch (err) {
+    console.log(err)
+  }
+}
+
+// Async functions return PROMISES - so we need to use .then() to get the data
+const data = asyncApiCall().then((data) => console.log(data))
+console.log(data) // Promise { <pending> } - because async functions return promises - so we need to use .then() to get the data
--- StripComments/stripComments.js
+++ StripComments/stripComments.js
@@ -1,15 +1,1 @@
-function solution(input, markers) {
-  // split input
-  // console.log(input)
-  const indexOfMarker = input.indexOf(markers[0]) // index of marker
 
-  // // console.log(indexOfMarker)
-  let modifiedString = input.substring(0, indexOfMarker)
-  console.log(modifiedString.trim())
-
-  // delete all elements
-}
-
-solution("apples, pears # and bananas", "#")
-
-// console.log("apple".substring(0, 3))
