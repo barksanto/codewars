252e1887c6e10dfafe89f3fd4249560476eddae6
Barkley <barkley.santo@gmail.com>
2022-12-02 14:13:39 +0800

    log total number fo args fnction

---
+++ SumAndMultiply/index.js
@@ -0,0 +1,8 @@
+// Write a function that accepts two parameters (sum and multiply)
+// and find two numbers[x, y], where x + y = sum and x * y = multiply.
+
+const numberOfArgs = (args) => {
+  console.log(args.length)
+}
+
+numberOfArgs([12, 36])
